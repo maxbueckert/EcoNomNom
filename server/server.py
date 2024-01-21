6d710028b43e7e4ed39858001a6b690ed80a53cd
MeganSorenson <85324120+MeganSorenson@users.noreply.github.com>
2024-01-20 23:19:41 -0800

    comments to server

--- server/server.py
+++ server/server.py
@@ -23,18 +23,27 @@ def recommend():
 
     # code for handling request
     assistants = GPTHandler()
+    print('Created assistants \n')
     # may need to chnage depending on frontend
     raw_text = request.args['web_text']
+    print('Got param\n')
+    print(raw_text)
     # raw_text = 'This is a recipe called the best chicken wings ever and i love it a lot and it is 1000 calories. The ingredients that you need are: 2 pounds of chicken wings, 1/2 cup barbeque sauce, 1/2 tsp salt, 2 stems of cilatro.'
     processed_text = HTMLTextProcessor.process_raw_html_text(raw_text)
+    print('Processe text\n')
     parsed_recipe = assistants.ask_assistant(processed_text, 'parser')
+    print('Parsed Recipe\n')
     original_recipe_json = assistants.ask_assistant(parsed_recipe, 'emissions')
+    print('Got Original Recipe\n')
     optimized_recipe_json = assistants.ask_assistant(
         original_recipe_json, 'optimizer')
+    print('Got Optimized Recipe\n')
     veg_recipe_json = assistants.ask_assistant(
         original_recipe_json, 'veg')
+    print('Got Vegetarian Recipe\n')
     vegan_recipe_json = assistants.ask_assistant(
         original_recipe_json, 'vegan')
+    print('Got Vegan Recipe\n')
 
     original_recipe_dict = GPTHandler.format_gpt_response(
         original_recipe_json)
@@ -44,12 +53,14 @@ def recommend():
         veg_recipe_json)
     vegan_recipe_dict = GPTHandler.format_gpt_response(
         vegan_recipe_json)
+    print('Converted all recipes to dicts\n')
 
     ultimate_dict = {}
     ultimate_dict['original'] = original_recipe_dict
     ultimate_dict['optimized'] = optimized_recipe_dict
     ultimate_dict['vegetarian'] = veg_recipe_dict
     ultimate_dict['vegan'] = vegan_recipe_dict
+    print('Compiled all recipes\n')
 
     return ultimate_dict
 
